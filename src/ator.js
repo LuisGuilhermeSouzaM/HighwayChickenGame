e97df14b53b503652fbdad0edc523d1c995e2888
LuisGuilherme-SM <81170691+LuisGuilhermeSouzaM@users.noreply.github.com>
2022-12-12 15:57:30 -0300

    finalizando curso de laços e listas com js

--- src/ator.js
+++ src/ator.js
@@ -15,7 +15,10 @@ function movimentaAtor(){
     }
     
     if (keyIsDown(DOWN_ARROW)){
-        yAtor += 3;
+        if(podeSeMover()){
+            yAtor += 3;
+        }
+        
     }
 }
 
@@ -24,6 +27,11 @@ function verificaColisao(){
         colisao = collideRectCircle(xCarros[i], yCarros[i], comprimentoCarro, alturaCarro, xAtor, yAtor, 15);
         if (colisao) {
             voltaAtorParaPosicaoInicial();
+            somDaColisao.play();
+            if (pontosMaiorQueZero()){
+                meusPontos--;
+            }
+            
         }
     }
 }
@@ -42,6 +50,15 @@ function incluiPontos(){
 function marcaPonto(){
     if (yAtor < 15){
         meusPontos++;
+        somDoPonto.play();
         voltaAtorParaPosicaoInicial();
     }
+}
+
+function pontosMaiorQueZero(){
+    return meusPontos > 0;
+}
+
+function podeSeMover(){
+    return yAtor < 366;
 }
--- src/images.js
+++ src/images.js
@@ -5,6 +5,11 @@ let imagemDoAtor;
 let imagemCarro;
 let imagemCarro2;
 let imagemCarro3;
+let imagemCarros;
+
+let somDaTrilha;
+let somDaColisao;
+let somDoPonto;
 
 function preload(){
     imagemDaEstrada = loadImage("../assets/images/estrada.png");
@@ -13,4 +18,7 @@ function preload(){
     imagemCarro2 = loadImage("../assets/images/carro-2.png");
     imagemCarro3 = loadImage("../assets/images/carro-3.png");
     imagemCarros = [imagemCarro, imagemCarro2, imagemCarro3, imagemCarro, imagemCarro2, imagemCarro3];
+    somDaTrilha = loadSound("../assets/sounds/trilha.mp3");
+    somDaColisao = loadSound("../assets/sounds/colidiu.mp3");
+    somDoPonto = loadSound("../assets/sounds/pontos.wav");    
 }
